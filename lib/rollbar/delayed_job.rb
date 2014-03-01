5d0821fd2bacc2499896938c921d58f192a8b954
Derek Rockwell <derek.a.rockwell@gmail.com>
2014-03-01 13:40:58 -0600

    merge up to 1.2.7

--- lib/rollbar/delayed_job.rb
+++ lib/rollbar/delayed_job.rb
@@ -3,7 +3,7 @@ module Rollbar
     def self.wrap_worker
       return if @wrapped
       @wrapped = true
-      
+
       ::Delayed::Worker.lifecycle.around(:invoke_job) do |job, *args, &block|
         begin
           block.call(job, *args)
@@ -11,8 +11,8 @@ module Rollbar
           if job.attempts >= ::Rollbar.configuration.dj_threshold
             data = ::Rollbar.configuration.report_dj_data ? job : nil
             ::Rollbar.report_exception(e, data)
+            # based on http://bit.ly/VGdfVI
           end
-          raise e
         end
       end
     end
